10c93196eb27cb6bc3c9fb5a34a7f2755f2d470a
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2022-12-31 18:43:37 +0400

    12-31

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let features_boxes = document.querySelectorAll(".features-box");
+let features_details = document.querySelectorAll(".features-details");
+let current_feature_index = 0;
+
+function change (move_to) {
+    if (move_to == features_details.length)
+        move_to = 0;
+    else if (move_to < 0)
+        move_to = features_details.length - 1;
+
+    features_details[current_feature_index].classList.remove('active');
+    features_boxes[current_feature_index].classList.remove('active');
+    features_details[move_to].classList.add('active');
+    features_boxes[move_to].classList.add('active');
+
+    current_feature_index = move_to;
+}
+
+features_boxes.forEach((box, box_index) => {
+    box.addEventListener('click', function () {
+        if (current_feature_index !== box_index)
+            change(box_index);
+    });
+});
